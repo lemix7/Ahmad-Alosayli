3d66f85da20190e0be3184c72b2cc530d61c8c5b
Ahmad <ahmedgeger@gmail.com>
2026-09-06 14:16:05 +0300

    Remove the smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Hero } from './components/Hero';
-import { About } from './components/About';
-import { Projects } from './components/Projects';
-import { Footer } from './components/Footer';
-import { SmoothScroll } from './components/SmoothScroll';
+import React from "react";
+import { Hero } from "./components/Hero";
+import { About } from "./components/About";
+import { Projects } from "./components/Projects";
+import { Footer } from "./components/Footer";
+// import { SmoothScroll } from './components/SmoothScroll';
 
 export const App: React.FC = () => {
   return (
-    <SmoothScroll>
-      <div className="min-h-screen  text-white">
-        <Hero />
-        <About />
-        <Projects />
-        <Footer />
-      </div>
-    </SmoothScroll>
+    // <SmoothScroll>
+    <div className="min-h-screen  text-white">
+      <Hero />
+      <About />
+      <Projects />
+      <Footer />
+    </div>
+    // </SmoothScroll>
   );
 };
 
 export default App;
-
